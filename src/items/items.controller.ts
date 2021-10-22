b27c6b4573a2c615412f19b46aeef8b68591626d
SabitTheBot <iamsabit99@gmail.com>
2021-10-22 21:37:28 +0600

    testing

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+
+    @Get()
+    findAll(): string {
+        return 'Get All Items';
+    }
+
+    @Post()
+    create(): string {
+        return 'Create Items'
+    }
+}
