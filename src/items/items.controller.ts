f1f5b18fcade4a91159a84be3c99eb085a83e79f
SabitTheBot <iamsabit99@gmail.com>
2021-10-25 21:13:02 +0600

    use findall from service

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
 
+    constructor(private readonly itemsService: ItemsService) {
+
+    }
+
     @Get()
-    findAll(): string {
-        return 'Get All Items';
+    findAll(): Item[] {
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
