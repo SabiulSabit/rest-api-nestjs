327294b33cf3445e70d22b0f071977abfc5506c5
SabitTheBot <iamsabit99@gmail.com>
2021-10-25 22:20:12 +0600

    find issue in service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './interfaces/item.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ItemsService {
-    //dummy data
-    private readonly items: Item[] = [
-        {
-            id: "adadwsew3232343434",
-            name: "item 1",
-            qty: 100,
-            description: "This is item one"
-        },
-        {
-            id: "adadws2ddfg32343434",
-            name: "item 2",
-            qty: 500,
-            description: "This is item two"
-        }
-    ]
+
+    constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) { }
 
     //find all items
-    findAll(): Item[] {
-        return this.items;
+    async findAll(): Promise<Item[]> {
+        return await this.itemModel.find();
     }
 
     //find one by id
-    findOne(id: string): Item {
-        return this.items.find(item => item.id === id)
+    async findOne(id: string): Promise<Item> {
+        return await this.itemModel.findOne({ _id: id })
     }
 }
