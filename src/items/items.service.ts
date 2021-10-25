48e4f7d059fe4a9538d44ae07eb143eac033b025
SabitTheBot <iamsabit99@gmail.com>
2021-10-25 21:06:16 +0600

    add static data in service page

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [
+        {
+            id: "adadwsew3232343434",
+            name: "item 1",
+            qty: 100,
+            description: "This is item one"
+        },
+        {
+            id: "adadws2ddfg32343434",
+            name: "item 2",
+            qty: 500,
+            description: "This is item two"
+        }
+    ]
+}
